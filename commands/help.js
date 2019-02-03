3c613d0f0737d5f5a4fdef31f8871199772d74c1
Edqe <configxgt@gmail.com>
2019-02-04 00:56:53 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,13 +3,15 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   let musc = [">play (link) | Only Youtube Links for now", "**>pause | Pause current song**", ">resume | Resume paused song", "**>dc | Leave the voice channel**", ">queue | Queue List", "**>skip | Skip a song**", ">volume | Set the volume (0-200)"]
   let mod = [">ban (tag) (reason) | Ban a user", "**>kick (tag) (reason) | Kick a user**", ">purge (number) | Clear messages"]
-  let els = [">ping | Check your ping", ">help | Show this embed"]
+  let els = [">ping | Check your ping", "**>help | Show this embed**", ">ascii (text) | ASCII Art (No Emojis)"]
+  let img = [">pat <user> | Pat, pat :3"]
   
   let embed = new Discord.RichEmbed()
   .setColor("#07b1bc")
   .addField("__Prefix__", ">")
   .addField(":musical_note: __Music__", musc, true)
   .addField(":tools: __Moderation__", mod, true)
+  .addField(":frame_photo: __Images__", img, true)
   .addField(":small_blue_diamond: __Other__", els, true)
   .setFooter("Scotix | Alpha 1.1") 
   
