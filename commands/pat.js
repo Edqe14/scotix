1b38ad1f66484dea3c6938a68b54087cf11d6e4e
Edqe <configxgt@gmail.com>
2019-02-04 00:18:14 +0700

    Update pat.js

--- commands/pat.js
+++ commands/pat.js
@@ -1,21 +1,38 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, ops) => {
-  let target = message.mentions.users.first();
-  if(!target) return message.channel.send("Who you want to pat? :3");
-  
   const dirr = [
     "https://media1.tenor.com/images/116fe7ede5b7976920fac3bf8067d42b/tenor.gif?itemid=9200932",
-    "https://media.tenor.com/images/fa9ad7f4ecfad744aec37241cce2cecc/tenor.gif"
+    "https://media1.tenor.com/images/5466adf348239fba04c838639525c28a/tenor.gif?itemid=13284057",
+    "https://zippy.gfycat.com/FantasticEmptyBluewhale.gif",
+    "https://giant.gfycat.com/FabulousCavernousAegeancat.gif",
+    "https://i.redd.it/zu24537rcsd21.png",
+    "https://media1.tenor.com/images/0cf73e99f7e58215b2deee3bdb158fe2/tenor.gif?itemid=12018811",
+    "https://media.tenor.com/images/37e36267891a33899bb2d88232f9f637/tenor.gif"
   ]
+  
+  let target = message.mentions.users.first();
+  if(!target) {
+    let embed = new Discord.RichEmbed()
+    .setColor("#07b1bc")
+    .setTitle(`There, there ${message.author.tag}`)
+    .setFooter("Scotix | Alpha 1.1") 
+    .setImage(dirr[Math.floor(Math.random() * dirr.length)])
+  
+    message.channel.send(embed);
+  } else {
+    let embed = new Discord.RichEmbed()
+    .setColor("#07b1bc")
+    .setTitle(`${message.author.tag} pats ${target.tag}`)
+    .setFooter("Scotix | Alpha 1.1") 
+    .setImage(dirr[Math.floor(Math.random() * dirr.length)])
 
-  let embed = new Discord.RichEmbed()
-  .setColor("#07b1bc")
-  .setTitle(`${message.author.tag} pats ${target}`)
-  .setFooter("Scotix | Alpha 1.1") 
-  .setImage(dirr[Math.floor(Math.random() * dirr.length)])
+    message.channel.send(embed);
+  }
+  
+  
 
-  message.channel.send(embed);
+  
 }
 
 module.exports.help = {
