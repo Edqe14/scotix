a0f99413cc5bbd151af5426dd5898329cd2a153d
Edqe <configxgt@gmail.com>
2019-02-03 23:25:47 +0700

    Update pat.js

--- commands/pat.js
+++ commands/pat.js
@@ -1,24 +1,19 @@
 const Discord = require("discord.js");
-const fs = require("fs")
+const image = require("get-image")
 
 module.exports.run = async (bot, message, args, ops) => {
   let target = message.guild.member(message.mentions.users.first());
   if(!target) return message.channel.send("Who you want to pat? :3");
   
-  fs.readdir("./pat/", (err, files) => {
-    if (err) console.error(err);
-    let giffiles = files.filter(f => f.split(".").pop() === "gif");
-  
-    if (giffiles.length <= 0) return console.log("There are no gifs to load...");
-    
-    let rand = Math.floor(Math.random() * f)
-    let fileSend = message.channel.sendFile(rand)
-  });
+  const dirr = [
+    "https://media1.tenor.com/images/116fe7ede5b7976920fac3bf8067d42b/tenor.gif?itemid=9200932",
+    "https://media.tenor.com/images/fa9ad7f4ecfad744aec37241cce2cecc/tenor.gif"
+  ]
 
   let embed = new Discord.RichEmbed()
   .setColor("#07b1bc")
   .setTitle(`${message.author.tag} pats ${target.tag}`)
-  .addField(`${fileSend}`);
+  .addField(dirr[Math.floor(Math.random() * dirr.length)])
 
   message.channel.send(embed)
 }
