7747fe7fb7a9bb2fa16213561002ea6f869ab8fb
Edqe <configxgt@gmail.com>
2019-02-03 21:59:26 +0700

    Update 0.1
    Added "pat" command
    Edited package.json

---
+++ commands/pat.js
@@ -0,0 +1,19 @@
+const randomPuppy = require('random-puppy');
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args, ops) => {
+  let target = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!target) return message.channel.send("You can't pat your own head, *smh*");
+  randomPuppy("headpats").then(url => {
+      let embed = new Discord.RichEmbed()
+      .setColor("#07b1bc")
+      .setTitle(`${message.author} pats ${target}`)
+      .addField(`${url}`);
+
+      message.channel.send(embed)
+  })
+}
+
+module.exports.help = {
+    name: "pat"
+  };
