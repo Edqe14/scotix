9acfdbb7e25395853741b52e875b6717dfca351a
Edqe <configxgt@gmail.com>
2019-02-03 22:12:11 +0700

    Update pat.js

--- commands/pat.js
+++ commands/pat.js
@@ -7,10 +7,11 @@ module.exports.run = async (bot, message, args, ops) => {
   randomPuppy("headpats").then(url => {
       let embed = new Discord.RichEmbed()
       .setColor("#07b1bc")
-      .setTitle(`${message.author} pats ${target}`)
+      .setTitle(`${message.author.tag} pats ${target.tag}`)
       .addField(`${url}`);
 
       message.channel.send(embed)
+      message.channel.send(`${url}`)
   })
 }
 
