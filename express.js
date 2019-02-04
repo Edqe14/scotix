04a63fd6826bd0ad9f46cb3609399da37127aca7
Edqe <configxgt@gmail.com>
2019-02-04 21:40:29 +0700

    f

--- express.js
+++ express.js
@@ -6,8 +6,8 @@ app.get('/', function(req, res) {
     res.json({
         stat: botjs.stats.online
     });
+    res.render("index")
 });
-
 
 app.listen(8080, function() {
     console.log("App listening on port 8080");
