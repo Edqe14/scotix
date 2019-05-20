7664ef6c6d61b04a79503ffb45584ae2705a2039
Edqe <34704796+ZeroTimez@users.noreply.github.com>
2019-05-20 21:36:43 +0700

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -5,12 +5,21 @@ module.exports.run = async(bot, message, args, ops) => {
   if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("Insufficient permission: `MANAGE_SERVER`")
   
   let prefixes = JSON.parse(fs.readFileSync("/app/prefixes.json", 'utf8'))
+
+  let prefix = prefixes[message.guild.id].prefixes
+  let str = ""
   
+  if(prefix) {
+    str = prefix
+  } else {
+    str = ">"
+  }
+  
+  if(!args[0] || args[0] == "help") return message.reply(`Usage: ${str}prefix (desired prefix)`)
+
   prefixes[message.guild.id] = {
     prefixes: args[0]
   };
-  
-  if(!args[0] || args[0] == "help") return message.reply(`Usage: ${prefixes[message.guild.id].prefixes}prefix (desired prefix)`)
   
   fs.writeFile("/app/prefixes.json", JSON.stringify(prefixes), (err) => {
     if(err) console.log(err)
