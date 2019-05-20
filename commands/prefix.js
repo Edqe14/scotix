0a4b3812bc0b3ef0a0236873b178e2c733dd64c3
Edqe <34704796+ZeroTimez@users.noreply.github.com>
2019-05-20 21:32:52 +0700

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,15 +1,16 @@
 const Discord = require("discord.js")
 const fs = require("fs")
 
-module.exports.run = async(bot, message, args, ops, prefix) => {
+module.exports.run = async(bot, message, args, ops) => {
   if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("Insufficient permission: `MANAGE_SERVER`")
-  if(!args[0] || args[0] == "help") return message.reply(`Usage: ${prefix}prefix (desired prefix)`)
   
   let prefixes = JSON.parse(fs.readFileSync("/app/prefixes.json", 'utf8'))
   
   prefixes[message.guild.id] = {
     prefixes: args[0]
   };
+  
+  if(!args[0] || args[0] == "help") return message.reply(`Usage: ${prefixes[message.guild.id].prefixes}prefix (desired prefix)`)
   
   fs.writeFile("/app/prefixes.json", JSON.stringify(prefixes), (err) => {
     if(err) console.log(err)
