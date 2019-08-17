e85bb8250da5e5e56941fe8084c99dfeec7610e9
Edqe <34704796+Edqe14@users.noreply.github.com>
2019-08-17 19:58:02 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,15 +1,19 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  let musc = [">(p)lay (link/title) | Play/Search a song", "**>pause | Pause current song**", ">(r)esume | Resume paused song", "**>dc | Leave the voice channel**", ">(q)ueue | Queue List", "**>(s)kip | Skip a song**", ">(v)olume | Set the volume (0-200)"]
-  let mod = [">ban (tag) (reason) | Ban a user", "**>kick (tag) (reason) | Kick a user**", ">purge (number) | Clear messages"]
-  let els = [">ping | Check your ping", "**>help | Show this embed**", ">ascii (text) | ASCII Art (No Emojis)", "**>poll (option) | Do a poll! (devide options with ;, 5 options)**"]
-  let img = [">pat <user> | Pat, pat :3", "**>avatar <user> | Get your/users avatar**"]
-  let inf = [">serverinfo | Get a server information", "**>uptime | Show Scotix uptime**",  ">google (keyword) | Search provided keyword on Google"]
+  let prefixes = JSON.parse(fs.readFileSync("/app/prefixes.json", "utf8"))
+  
+  let prefix = prefixes[message.guild.id].prefixes
+  
+  let musc = [`${prefix}(p)lay (link/title) | Play/Search a song`, `**${prefix}pause | Pause current song**`, `${prefix}(r)esume | Resume paused song`, `**${prefix}dc | Leave the voice channel**`, `${prefix}(q)ueue | Queue List`, `**${prefix}(s)kip | Skip a song**`, `${prefix}(v)olume | Set the volume (0-200)`]
+  let mod = [`${prefix}ban (tag) (reason) | Ban a user`, `**${prefix}kick (tag) (reason) | Kick a user**`, `${prefix}purge (number) | Clear messages`]
+  let els = [`${prefix}ping | Check your ping`, `**${prefix}help | Show this embed**`, `${prefix}ascii (text) | ASCII Art (No Emojis)`, `**${prefix}poll (option) | Do a poll! (devide options with \`;\`, devide question with \`-\`, max. 5 options)**`]
+  let img = [`${prefix}pat <user> | Pat, pat :3`, `**${prefix}avatar <user> | Get your/users avatar**`]
+  let inf = [`${prefix}serverinfo | Get a server information`, `**${prefix}uptime | Show Scotix uptime**`] //,  `${prefix}google (keyword) | Search provided keyword on Google`]
   
   let embed = new Discord.RichEmbed()
   .setColor("#07b1bc")
-  .addField("__Prefix__", ">")
+  .addField("__Prefix__", prefix)
   .addField(":musical_note: __Music__", musc, true)
   .addField(":tools: __Moderation__", mod, true)
   .addField(":frame_photo: __Images__", img, true)
